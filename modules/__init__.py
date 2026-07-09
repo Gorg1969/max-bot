12df7929fbb52851780366d38af83ff67f4714fe
Gorg1969 <gorgnik@gmail.com>
2026-07-09 12:59:16 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,9 +1,6 @@
-"""
-Модули для MAX бота
-"""
-from .google_drive import GoogleDriveStorage
+from .google_drive import GoogleDrive
 from .publisher import Publisher
 from .scheduler import Scheduler
 from .user_state import UserState
 
-__all__ = ['GoogleDriveStorage', 'Publisher', 'Scheduler', 'UserState']
+__all__ = ['GoogleDrive', 'Publisher', 'Scheduler', 'UserState']
