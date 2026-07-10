70756f6b460ae0550803560627fb64f8615372c1
Gorg1969 <gorgnik@gmail.com>
2026-07-10 13:59:03 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -4,5 +4,16 @@ from .publisher import Publisher
 from .web_interface import WebInterface
 from .user_auth import UserAuth
 from .google_drive import GoogleDrive
+from .process_links import extract_file_id_from_url, download_file_from_drive, process_google_drive_link
 
-__all__ = ['Database', 'FileManager', 'Publisher', 'WebInterface', 'UserAuth', 'GoogleDrive']
+__all__ = [
+    'Database',
+    'FileManager',
+    'Publisher',
+    'WebInterface',
+    'UserAuth',
+    'GoogleDrive',
+    'extract_file_id_from_url',
+    'download_file_from_drive',
+    'process_google_drive_link'
+]
