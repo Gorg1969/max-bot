2729b871316c4043dbf313a74c0abd99e0d888d2
Gorg1969 <gorgnik@gmail.com>
2026-07-10 10:15:38 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,5 +2,7 @@ from .database import Database
 from .file_manager import FileManager
 from .publisher import Publisher
 from .web_interface import WebInterface
+from .user_auth import UserAuth
+from .google_drive import GoogleDrive
 
-__all__ = ['Database', 'FileManager', 'Publisher', 'WebInterface']
+__all__ = ['Database', 'FileManager', 'Publisher', 'WebInterface', 'UserAuth', 'GoogleDrive']
