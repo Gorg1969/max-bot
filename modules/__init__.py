4a0c2882aa0118d10a40f63671fe4a83e773ba4d
Gorg1969 <gorgnik@gmail.com>
2026-07-15 15:13:07 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -2,24 +2,12 @@ from .database import Database
 from .file_manager import FileManager
 from .publisher import Publisher
 from .web_interface import WebInterface
-from .process_links import (
-    extract_file_id_from_url,
-    convert_to_direct_link,
-    download_file_from_drive,
-    process_google_drive_link
-)
 from .report_generator import ReportGenerator
-from .download_handler import DownloadHandler
 
 __all__ = [
     'Database',
     'FileManager',
     'Publisher',
     'WebInterface',
-    'ReportGenerator',
-    'DownloadHandler',
-    'extract_file_id_from_url',
-    'convert_to_direct_link',
-    'download_file_from_drive',
-    'process_google_drive_link'
+    'ReportGenerator'
 ]
