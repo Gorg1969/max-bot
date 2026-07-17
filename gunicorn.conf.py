189152a54c6c135d02bb7f177410d5f488537929
Gorg1969 <gorgnik@gmail.com>
2026-07-17 13:45:14 +0500

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,15 +1,14 @@
 # gunicorn.conf.py
 
 import os
-import multiprocessing
 
 # Настройки Gunicorn
 bind = "0.0.0.0:" + os.environ.get("PORT", "3000")
-workers = int(os.environ.get("WEB_CONCURRENCY", 4))
+workers = int(os.environ.get("WEB_CONCURRENCY", 2))
 worker_class = "gthread"
 threads = int(os.environ.get("THREADS", 2))
 worker_connections = 1000
-timeout = 120
+timeout = 600  # 10 минут
 keepalive = 5
 max_requests = 1000
 max_requests_jitter = 100
@@ -23,4 +22,4 @@ loglevel = os.environ.get("LOG_LEVEL", "info")
 preload_app = True
 
 def post_fork(server, worker):
-    server.log.info(f"Worker spawned (pid: {worker.pid})")
+    server.log.info(f"✅ Worker spawned (pid: {worker.pid})")
