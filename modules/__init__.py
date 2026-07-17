9d1969e1d756aeb529461a547359501329ce9fef
Gorg1969 <gorgnik@gmail.com>
2026-07-17 09:46:12 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,16 +1,25 @@
 # modules/__init__.py
+
 from .database import Database
 from .file_manager import FileManager
 from .publisher import Publisher
-from .web_interface import WebInterface
 from .report_generator import ReportGenerator
-from .session_manager import SessionManager
+from .tasks import process_folder_task, cleanup_user_task
+
+# Добавляем APIClient для использования в воркере
+class APIClient:
+    def __init__(self):
+        import os
+        import requests
+        self.token = os.environ.get("MAX_TOKEN") or os.environ.get("MAX_BOT_TOKEN") or os.environ.get("TOKEN")
+        self.base_url = "https://platform-api2.max.ru"
 
 __all__ = [
     'Database',
     'FileManager',
     'Publisher',
-    'WebInterface',
     'ReportGenerator',
-    'SessionManager'
+    'APIClient',
+    'process_folder_task',
+    'cleanup_user_task'
 ]
