0184cf3df0e75e18dd5a26209357ac3f14a38622
Gorg1969 <gorgnik@gmail.com>
2026-07-19 08:50:27 +0500

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,13 +1,12 @@
+# modules/__init__.py
 from .database import Database
 from .file_manager import FileManager
 from .publisher import Publisher
-from .web_interface import WebInterface
 from .report_generator import ReportGenerator
 
 __all__ = [
     'Database',
     'FileManager',
     'Publisher',
-    'WebInterface',
     'ReportGenerator'
 ]
